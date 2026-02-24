233550e04063e7358d841aca3db13dcff08da988
Shiva Tekumalla <shivat2@illinois.edu>
2026-02-23 21:26:47 -0600

    Updating Some React Stuff After Changing Versions

--- src/gui/Overlay.jsx
+++ src/gui/Overlay.jsx
@@ -1,64 +1,67 @@
 import React, { Component } from "react";
-import Loader from "react-loader-spinner";
+import { Watch } from "react-loader-spinner";
 import Settings from "./Settings";
 import bus from "../events";
 import styles from "./Overlay.module.css";
 import classNames from "classnames";
 
 export default class Overlay extends Component {
-	state = { isSettingsMenuOpen: false, isLoading: false, message: null };
+  state = { isSettingsMenuOpen: false, isLoading: false, message: null };
 
-	render() {
-		const { isVisible } = this;
-		const { isSettingsMenuOpen, isLoading, message } = this.state;
+  render() {
+    const { isVisible } = this;
+    const { isSettingsMenuOpen, isLoading, message } = this.state;
 
-		return (
-			<div className={classNames(styles.overlay, isVisible && styles.show)}>
-				<div
-					className={classNames(styles.loader, message == null && styles.dark)}
-				>
-					{isSettingsMenuOpen ? (
-						<Settings />
-					) : message != null ? (
-						<div className={styles.message}>{message}</div>
-					) : (
-						isLoading && (
-							<Loader type="Watch" color="#CCCCCC" height="50" width="50" />
-						)
-					)}
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div className={classNames(styles.overlay, isVisible && styles.show)}>
+        <div className={classNames(styles.loader, message == null && styles.dark)}>
+          {isSettingsMenuOpen ? (
+            <Settings />
+          ) : message != null ? (
+            <div className={styles.message}>{message}</div>
+          ) : (
+            isLoading && (
+              <Watch
+                height="50"
+                width="50"
+                color="#CCCCCC"
+                ariaLabel="loading"
+              />
+            )
+          )}
+        </div>
+      </div>
+    );
+  }
 
-	componentDidMount() {
-		bus
-			.on("isLoading", (isLoading) => {
-				this.setState({ isLoading });
-			})
-			.on("message", (message) => {
-				this.setState({ isLoading: false, message });
-			})
-			.on("openSettings", () => {
-				this.setState({ isSettingsMenuOpen: true });
-			})
-			.on("closeSettings", () => {
-				this.setState({ isSettingsMenuOpen: false });
-			});
-	}
+  componentDidMount() {
+    bus
+      .on("isLoading", (isLoading) => {
+        this.setState({ isLoading });
+      })
+      .on("message", (message) => {
+        this.setState({ isLoading: false, message });
+      })
+      .on("openSettings", () => {
+        this.setState({ isSettingsMenuOpen: true });
+      })
+      .on("closeSettings", () => {
+        this.setState({ isSettingsMenuOpen: false });
+      });
+  }
 
-	componentWillUnmount() {
-		bus.removeListener("isLoading");
-		bus.removeListener("message");
-		bus.removeListener("openSettings");
-		bus.removeListener("closeSettings");
-	}
+  componentWillUnmount() {
+    bus.removeListener("isLoading");
+    bus.removeListener("message");
+    bus.removeListener("openSettings");
+    bus.removeListener("closeSettings");
+  }
 
-	get isVisible() {
-		return (
-			this.state.isSettingsMenuOpen ||
-			this.state.isLoading ||
-			this.state.message != null
-		);
-	}
+  get isVisible() {
+    return (
+      this.state.isSettingsMenuOpen ||
+      this.state.isLoading ||
+      this.state.message != null
+    );
+  }
 }
