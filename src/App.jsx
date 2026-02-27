ba6069e8a92370c13900974bcee6afacd18cec5b
Shiva Tekumalla <shivat2@illinois.edu>
2026-02-26 20:01:37 -0600

    Enabling Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,32 @@ import querystring from "query-string";
 import { Analytics } from "@vercel/analytics/react";
 
 export default class App extends PureComponent {
-	render() {
-		const route = window.location.hash;
+  render() {
+    const route = window.location.hash;
 
-		if (route.startsWith("#/join"))
-			return <PlayScreen token={this.inviteToken} />;
-		else return <PlayScreen />;
-	}
+    const screen = route.startsWith("#/join")
+      ? <PlayScreen token={this.inviteToken} />
+      : <PlayScreen />;
 
-	UNSAFE_componentWillMount() {
-		this._listener = window.addEventListener("hashchange", (e) => {
-			this.forceUpdate();
-		});
-	}
+    return (
+      <>
+        {screen}
+        <Analytics />
+      </>
+    );
+  }
 
-	componentWillUnmount() {
-		window.removeEventListener("hashchange", this._listener);
-	}
+  UNSAFE_componentWillMount() {
+    this._listener = window.addEventListener("hashchange", (e) => {
+      this.forceUpdate();
+    });
+  }
 
-	get inviteToken() {
-		return querystring.parse(window.location.search).token;
-	}
+  componentWillUnmount() {
+    window.removeEventListener("hashchange", this._listener);
+  }
+
+  get inviteToken() {
+    return querystring.parse(window.location.search).token;
+  }
 }
